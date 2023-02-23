682c304346e0f764ea7ae15175751f7ecb6fd3cb
Afolabi <Afolabi>
2023-02-22 16:48:22 -0800

    files creation

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,6 @@
-int _putchar(char c)
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,3 +14,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
+#endif
