dd29490cb6f76cdc07e8fe177c78ddd49172f91f
Afolabi <Afolabi>
2023-02-22 17:11:17 -0800

    update4

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,15 +4,17 @@
  * print_alphabet_x10 - print the alphabets in lower case ten times
  * followed by a new line
  */
-void print_alphabet_x10(void);
+
+void print_alphabet_x10(void)
 {
 	int number = 0;
 	char ch;
 
 	while (number <= 9)
 	{
-		for (ch = 'a'; ch <= 'z; ch++}
+		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
 		_putchar('\n');
 		}
+}
 
