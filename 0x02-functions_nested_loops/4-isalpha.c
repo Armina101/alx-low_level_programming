d70ec387f045364f617955daa6ee6667d8f388f9
Afolabi <Afolabi>
2023-02-22 18:07:32 -0800

    update7

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,11 +4,11 @@
  * _isalpha - checking for char alphabet
  * @c: char to be checked
  *
- * Return: 1 if char is lower or upper else 0 otherwise
+ * Return: 1 if char is lower or upper, 0 otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
