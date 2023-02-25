e3082e3950dba8aa2843210754858d3eccc16abb
Afolabi <Afolabi>
2023-02-24 19:54:41 -0800

    update5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers: prints 0 to 9 except 2 and 4
+ * print_most_numbers -  prints 0 to 9
+ * (neither 2 nor 4)
  * followed by a new line
  * Return: void
  */
