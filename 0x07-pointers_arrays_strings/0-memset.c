5c4a0c9ed74d33621992040c7605cabb15a6c295
Afolabi <Afolabi>
2023-03-06 12:37:05 -0800

    update

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory adress
+ * @b: constant byte
+ * @n: bytes of the memory
+ * Return: the pointer to dest
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+		*(s + a) = b;
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination area
+ * @src: source area
+ * @n: bytes
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+		*(dest + a) = *(src + a);
+
+	return (dest);
+}
