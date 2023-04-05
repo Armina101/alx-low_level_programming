022ecbe00d025357fbf17420095f5dc7105c3867
Afolabi <Afolabi>
2023-04-04 17:13:46 -0700

    Files creation for advanced tasks

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * reverse_listint - pointer
+ * @head: pointer
+ * Return: pointer to the first node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+
+}
