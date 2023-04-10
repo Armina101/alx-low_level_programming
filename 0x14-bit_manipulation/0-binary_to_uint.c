4b68a0ce74e9ff2939b833aae156702a548c03cb
Afolabi <Afolabi>
2023-04-10 08:55:44 -0700

    Creation of README and Header File

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * int binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to a string of 0 and 1 chars
+ * Return: the converted number,
+ * 0 if there is one or more chars in the string b that is not 0 or 1
+ * 0 if b is NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+Learning about Bitwise Operators and Bit Manipulation
