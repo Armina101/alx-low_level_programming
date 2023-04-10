cd06ec22ddd5f588af9986eedbad99ce013ede2e
Afolabi <Afolabi>
2023-04-10 15:24:33 -0700

    Creation of More Functions

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,7 @@
 #include "main.h"
 
 /**
- * int binary_to_uint - converts a binary number to an unsigned int
+ * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointer to a string of 0 and 1 chars
  * Return: the converted number,
  * 0 if there is one or more chars in the string b that is not 0 or 1
@@ -21,9 +21,8 @@ unsigned int binary_to_uint(const char *b)
 
 	while (b[a] != '\0')
 		a++;
-	/*
-	To find and get length of the string
-	*/
+	/*To find and get length of the string*/
+
 	for (a--, bin = 1; a >= 0; a--, bin *= 2)
 	{
 		if (b[a] != '0' && b[a] != '1')
