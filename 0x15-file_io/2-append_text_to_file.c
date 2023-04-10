c7b23d1ef0829d48ccc519f2f44236273ab2e9a7
Afolabi <Afolabi>
2023-04-10 08:44:39 -0700

    Creation of Files

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end of the file
+ * Return: 1 on success and -1 on failure
+ * If filename is NULL return -1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+}
