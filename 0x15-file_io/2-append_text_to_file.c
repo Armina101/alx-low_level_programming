b8bf74b0925babc0c834ffbc96d53cc81b220b81
Afolabi <Afolabi>
2023-04-10 14:37:00 -0700

    More Debugging

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,5 +13,26 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int file_desc, n, file_write;
 
+	n = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_desc = open(filename, O_WRONLY | O_APPEND);
+
+	if (file_desc < 0)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	while (text_content[n])
+		n++;
+	file_write = write(file_desc, text_content, n);
+	if (file_write < 0)
+		return (-1);
+	close(file_desc);
+	return (1);
 }
