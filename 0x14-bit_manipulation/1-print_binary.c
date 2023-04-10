ed23335d65210341b729c4f4dae06c9b8cae3bc8
Afolabi <Afolabi>
2023-04-10 09:08:07 -0700

    Creation of Files

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+
+}
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @index: the index, starting from 0 of the bit you want to get
+ * @n: value of bit
+ * Return: value of the bit at index index or -1 if an error occurs
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+
+}
